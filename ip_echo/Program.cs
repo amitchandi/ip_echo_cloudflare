938a38c8a400aa1eb423185f22dff7d62607b352
Amit Chandi <chandi.amit97@gmail.com>
2025-02-18 05:53:43 -0800

    init

---
+++ ip_echo/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/", (HttpRequest request) => request.Host.Host)
+    .WithName("Get");
+
+app.Run();
