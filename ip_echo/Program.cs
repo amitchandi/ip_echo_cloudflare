35806191681f14b55c4f95aee1e8e6f7f2d952c2
Amit Chandi <chandi.amit97@gmail.com>
2025-10-25 02:41:55 -0700

    created /v6 endpoint for cloudflare ipv6 HTTP header

--- ip_echo/Program.cs
+++ ip_echo/Program.cs
@@ -15,4 +15,7 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", (HttpRequest request) => request.Headers["CF-CONNECTING-IP"].ToString())
     .WithName("Get");
 
+app.MapGet("/v6", (HttpRequest request) => request.Headers["CF-CONNECTING-IPv6"].ToString())
+    .WithName("GetV6");
+
 app.Run();
